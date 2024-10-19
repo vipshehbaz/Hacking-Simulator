9b17d6cc03a2c45ef2825772ade47ddd92ab86a9
Muhammad Shehbaz Khan <157197912+vipshehbaz@users.noreply.github.com>
2024-10-19 16:53:20 +0500

    upload files
    if you want to use this so 
    first you create one assets folder here and make sure folder name same as mine 
    second you create a video folder in assets folder and this folder name are also same as mine 
    third you download a video from anywhere and place it in the video folder and video name  is "Fake Hacking! Fake Virus! Pretend to be a Pro Hacker!.mp4" is your video is not same the video is not appear on web

---
+++ script/index.js
@@ -0,0 +1,23 @@
+let msg = document.getElementById("text");
+
+setTimeout(() => (msg.innerHTML = "Reading Your Files"), 3000);
+setTimeout(() => (msg.innerHTML = "Reading Your Files."), 3500);
+setTimeout(() => (msg.innerHTML = "Reading Your Files.."), 4000);
+setTimeout(() => (msg.innerHTML = "Reading Your Files..."), 4500);
+setTimeout(() => (msg.innerHTML = "Reading Your Files"), 5000);
+setTimeout(() => (msg.innerHTML = "Reading Your Files."), 5500);
+setTimeout(() => (msg.innerHTML = "Reading Your Files.."), 6000);
+setTimeout(() => (msg.innerHTML = "Reading Your Files..."), 6500);
+setTimeout(() => (msg.innerHTML = "Password Files Detected"), 7000);
+setTimeout(() => (msg.innerHTML = "Password Files Detected."), 7500);
+setTimeout(() => (msg.innerHTML = "Password Files Detected.."), 8000);
+setTimeout(() => (msg.innerHTML = "Password Files Detected..."), 8500);
+let send = setTimeout(() => (msg.innerHTML = "Sending All Files"), 9000);
+setTimeout(() => (msg.innerHTML = "Sending All Files."), 9500);
+setTimeout(() => (msg.innerHTML = "Sending All Files.."), 10000);
+setTimeout(() => (msg.innerHTML = "Sending All Files..."), 10500);
+
+let popup = document.getElementById("popup");
+
+setTimeout(() => (popup.style.display = "inline-block"), 1000);
+
